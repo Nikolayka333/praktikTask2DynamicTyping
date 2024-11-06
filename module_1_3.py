376180ed9716c48ab321dff63febc491e4188d3b
Nikolayka333 <nikolayka333@gmail.com>
2024-11-06 17:22:38 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,19 @@
+#вариант исполнения с выводом в консоль итоговых значений
+name = 'Nikolay'
+print(name)
+age = 31
+print(age)
+new_age = age + 1
+print(new_age)
+is_student = 'True' or "False"
+print(is_student)
+
+#вариант исполнения с выводом в консоль значений, показаных на примере
+name = 'Nikolay'
+print('Name: ' + name)
+age = 31
+print('Age: ' + str(age))
+new_age = (age + 1)
+print('New age: ' + str(new_age))
+is_student = 'True' or 'False'
+print('Is student: ' + str(is_student))
